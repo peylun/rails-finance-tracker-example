9e2fe73c90fa5eb80fa10523ef0975fa848c223e
PeyLun <peylun@gmail.com>
2018-01-22 23:28:32 -0500

    Updated stock search to ajax call

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,14 +2,8 @@ class StocksController < ApplicationController
   def search
     if params[:stock].present?
       @stock = Stock.new_from_lookup(params[:stock])
-      if @stock
-        render 'users/my_portfolio'
-      else
-        flash[:info] = "Stock \"#{params[:stock]}\" not found"
-        redirect_to my_portfolio_path
-      end
-    else
-      redirect_to my_portfolio_path
+      flash.now[:danger] = "Stock \"#{params[:stock]}\" not found" unless @stock
     end
+    render partial: 'users/result'
   end
 end
