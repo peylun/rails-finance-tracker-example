35c4b4d4fe6bf6f70aa9b2b6570af3be03e02abe
PeyLun <peylun@gmail.com>
2018-01-25 01:28:36 -0500

    Added friends search result

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,15 @@ class UsersController < ApplicationController
   end
   
   def my_friends
+    @friendships = current_user.friends
+  end
+  
+  def search
+    if params[:search_param].present?
+      @users = User.search(params[:search_param])
+      @users = current_user.except_current_user(@users)
+      flash.now[:danger] = "No matching records found" if @users.blank?
+    end
+    render partial: 'friends/result'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,12 @@ Rails.application.routes.draw do
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'my_friends', to: 'users#my_friends'
   get 'search_stocks', to: 'stocks#search'
+  get 'search_friends', to: 'users#search'
+  post 'add_friend', to: 'users#add_friend'
   
-  resources :stocks, only: [:destroy]
   resources :user_stocks, only: [:create, :destroy]
+  resources :users, only: [:show]
+  resources :friendships
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
